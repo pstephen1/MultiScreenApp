e940b601a17a508990da4befb45ebcc4f9bb0522
pstephen1 <pstephen1@live.com>
2022-04-30 11:49:53 -0400

    Add project files.

---
+++ MultiScreenApp/Page2.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiScreenApp
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page3());
+        }
+
+        private void PreviousPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page1());
+        }
+    }
+}
